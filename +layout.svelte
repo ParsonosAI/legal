42f71e56cad0a047f757e0b711ef315a1f1cf9c6
Ruben Kruepper <ruben.kruepper@gmail.com>
2024-11-25 20:41:15 +0100

    palaba: I'm so sorry

---
+++ +layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import type { Snippet } from "svelte"
+
+  interface Props {
+    children?: Snippet
+  }
+
+  let { children }: Props = $props()
+</script>
+
+<div class="container prose prose-stone mx-auto py-8 lg:prose-lg lg:py-16">
+  {@render children?.()}
+</div>
